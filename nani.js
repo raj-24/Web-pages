03247dc1006476db818ba153c5fc3d64aef27f02
raj-24 <72553700+raj-24@users.noreply.github.com>
2023-01-24 18:16:17 +0530

    Add files via upload

---
+++ nani.js
@@ -0,0 +1,23 @@
+let a=10
+let b=3
+// let c=a+b 
+  document.getElementById("demo").textContent=a;
+  document.getElementById("lemo").textContent=b;                       
+
+         function hav(){
+            let c=a+b
+
+ document.getElementById("cam").textContent = c 
+       }
+         function gav(){
+            let d=a-b
+document.getElementById("pamo").textContent = d
+         }
+         function pav(){
+          let e=a*b
+document.getElementById("cam").textContent = e
+       }
+       function cav(){
+        let f=a/b
+document.getElementById("pamo").textContent = f
+     }
